6ab80f0accc08feb268f3b8f78a59efbd1eb845b
pblgllgs <pbl.gllgs@gmail.com>
2023-11-16 12:38:49 -0300

    add ingress, services

--- kind/create-cluster.sh
+++ kind/create-cluster.sh
@@ -6,20 +6,18 @@ kind create cluster --config kind-config.yml
 
 echo "\n-------------------------------------------------\n"
 
-echo "Instalin NGINX Ingres"
+echo "Installing NGINX Ingress"
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 echo "\n-------------------------------------------------\n"
 
-echo "Waiting got NGINX Inggress to be ready..."
-
-sleep 10
+echo "Waiting got NGINX Ingress to be ready..."
 
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
-  --timeout=180s
+  --timeout=90s
 
 echo "\n"
 
